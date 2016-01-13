629c1090bbef7b359b70d8350b087c4d3e5664b1
Robert Dodier <robert_dodier@users.sourceforge.net>
2016-01-12 17:59:34 -0800

    Add test case to test class.

--- src/test/scala/org/robertdodier/wilcoxonSuite.scala
+++ src/test/scala/org/robertdodier/wilcoxonSuite.scala
@@ -23,5 +23,9 @@ class wilcoxonSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("compute scaled rank sum with made-up data") {
+    val scoresAndLabels = Seq((0.6, 1), (0.9, 1), (0.3, 1), (0.2, 0), (0.1, 0), (0.5, 0))
+    val U = wilcoxon.U (sc.parallelize (scoresAndLabels))
+    val expected_U = 8.0/9.0
+    assert (Math.abs (U - expected_U) <= 1e-12)
   }
 }
